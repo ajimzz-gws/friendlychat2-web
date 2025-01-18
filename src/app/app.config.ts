903981ce6badbc2a0a628c13f16e8f1d1d7bc36c
ajimzz-gws <azim.khairi7@gmail.com>
2025-01-19 01:09:30 +0800

    update 1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,24 +1,25 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
-import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
-import { provideFunctions, getFunctions, connectFunctionsEmulator} from '@angular/fire/functions';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
 import { provideMessaging, getMessaging } from '@angular/fire/messaging';
-import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 import { routes } from './app.routes';
 import { provideRouter } from '@angular/router';
 
+// Fix: Use `provideFirebaseApp` correctly with AngularFire services
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(
       provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
       provideAuth(() => getAuth()),
+      provideFirestore(() => getFirestore()),
       provideFunctions(() => getFunctions()),
       provideStorage(() => getStorage()),
       provideMessaging(() => getMessaging())
     ),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat2-a5761","appId":"1:230253882441:web:cc3306f1e9e71d22ef6398","storageBucket":"friendlychat2-a5761.firebasestorage.app","apiKey":"AIzaSyDeK_0hSeKdMDeGIFgaaARyhDipqzROwkQ","authDomain":"friendlychat2-a5761.firebaseapp.com","messagingSenderId":"230253882441"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+    provideRouter(routes),
   ],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,11 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDeK_0hSeKdMDeGIFgaaARyhDipqzROwkQ",
-  authDomain: "friendlychat2-a5761.firebaseapp.com",
-  projectId: "friendlychat2-a5761",
-  storageBucket: "friendlychat2-a5761.firebasestorage.app",
-  messagingSenderId: "230253882441",
-  appId: "1:230253882441:web:cc3306f1e9e71d22ef6398"
+export const environment = {
+  production: false,  // You can change this to true for production
+  firebase: {
+    apiKey: "AIzaSyDeK_0hSeKdMDeGIFgaaARyhDipqzROwkQ",
+    authDomain: "friendlychat2-a5761.firebaseapp.com",
+    projectId: "friendlychat2-a5761",
+    storageBucket: "friendlychat2-a5761.firebasestorage.app",
+    messagingSenderId: "230253882441",
+    appId: "1:230253882441:web:cc3306f1e9e71d22ef6398"
+  }
 };
